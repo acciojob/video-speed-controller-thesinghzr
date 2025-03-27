c657e60ef95244153d304673691d502530545b64
Suraj Singh <yousurajsingh@gmail.com>
2025-03-27 22:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector(".viewer");
+const toggle = document.querySelector(".toggle");
+const progress = document.querySelector(".progress__filled");
+const volumeInput = document.querySelector("input[name='volume']");
+const playbackSpeedInput = document.querySelector("input[name='playbackSpeed']");
+const rewindButton = document.querySelector(".rewind");
+const forwardButton = document.querySelector(".forward");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlayPause(){
+	if(video.paused || video.ended){
+		video.play();
+		toggle.textContent = " ❚ ❚";
+	}else{
+		video.pause();
+		toggle.textContent = "►";
+	}
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateProgress(){
+	const progressFilled = (video.currentTime/video.duration) * 100;
+	progress.style.width = `${progressFilled}%`;
+	if(video.ended){
+		toggle.textContent = "►";
+	}
+}
+
+function setVolume(){
+	video.volume = volumeInput.value;
+}
+
+function setPlaybackSpeed(){
+	video.playbackRate = playbackSpeedInput.value;
+}
+
+function skipVideo(event){
+	const skipDuration = Number(event.target.dataset.skip);
+	video.currentTime += skipDuration;
+}
+
+video.addEventListener("timeupdate",updateProgress);
+volumeInput.addEventListener("input",setVolume);
+playbackSpeedInput.addEventListener("input",setPlaybackSpeed);
+
+toggle.addEventListener("click",togglePlayPause);
